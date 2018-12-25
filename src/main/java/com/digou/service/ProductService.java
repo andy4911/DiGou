5d43352cc8f4337002285cf851fd40aee931bdd8
xuxiaojie@youzan.com <xuxiaojie@youzan.com>
2018-12-25 14:49:04 +0800

    合并分支解决冲突

--- src/main/java/com/digou/service/ProductService.java
+++ src/main/java/com/digou/service/ProductService.java
@@ -25,38 +25,11 @@ public class ProductService {
 	
 	@Resource
 	ProductMapper productMapper;
-	
-	public Map<String, Object> searchProducts(HttpServletResponse response, String pName, int pageIndex, int pageSize) {
-    	if (pageSize == 0) {
-			pageSize = 20;
-		} 
-		
-		ArrayList<Product> productArr = null;
-		if (pName == null || pName.equals("")) {
-			productArr = productMapper.findAll(pageIndex * pageSize, pageSize);
-		} else {
-			productArr = productMapper.find(pName, pageIndex * pageSize, pageSize);
-		}
-		
-		ArrayList<String> conditionArr = new ArrayList<String>();
-		conditionArr.add("pName");
-		conditionArr.add("price");
-		conditionArr.add("num");
-		conditionArr.add("shopName");
-		conditionArr.add("pID");
-		conditionArr.add("description");
-		conditionArr.add("portraitURL");
-		
-		Map<String, Object> data = ResponseCommon.filter(productArr, conditionArr, "pArray");
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("pageIndex", pageIndex);
-		return ResponseCommon.wrappedResponse(data, 101, map);
-//=======
-//	public Map<String, Object> searchProducts(HttpServletResponse response, String pName) {
-//		Map<String, Object> map = new HashMap<>();
-//		map.put("array", productMapper.find(pName));
-//		return ResponseCommon.wrappedResponse(map, 101, map);
-//>>>>>>> Stashed changes
-	}
+
+    public Map<String, Object> searchProducts(HttpServletResponse response, String pName) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("array", productMapper.find(pName));
+        return ResponseCommon.wrappedResponse(map, 101, map);
+    }
 
 }
